915a2b669eb8ba185778a695865cf3cfe09dbf1b
ArminKianian <arminkia.dev@gmail.com>
2023-06-09 11:47:26 +0430

    Updating the form to allow editing

--- BlazingTrails.Client/Features/ManageTrails/Shared/UploadTrailImageHandler.cs
+++ BlazingTrails.Client/Features/ManageTrails/Shared/UploadTrailImageHandler.cs
@@ -1,7 +1,7 @@
 ﻿using BlazingTrails.Shared.Features.ManageTrails;
 using MediatR;
 
-namespace BlazingTrails.Client.Features.ManageTrails
+namespace BlazingTrails.Client.Features.ManageTrails.Shared
 {
     public class UploadTrailImageHandler : IRequestHandler<UploadTrailImageRequest, UploadTrailImageRequest.Response>
     {
